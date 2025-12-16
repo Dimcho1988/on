5cdc0c0395740729370a8bec53880783bbd1393b
Dimcho1988 <dimchomitsov@gmail.com>
2025-12-16 18:44:31 +0100

    Delete cs_model.py

--- cs_model.py
+++
@@ -1,8 +0,0 @@
-def compute_cs(tt_results):
-    """
-    Изчислява Critical Speed от два теста.
-    tt_results: [{'distance': m, 'time': s}, ...]
-    """
-    d1, d2 = tt_results
-    cs = (d2["distance"] - d1["distance"]) / (d2["time"] - d1["time"])  # m/s
-    return {"cs": cs * 3.6}  # km/h
